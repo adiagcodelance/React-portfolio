fd57c2dc4f9deda675e8d2944a7b78175f54c6c4
aditya7496 <43040075+aditya7496@users.noreply.github.com>
2023-05-22 20:57:17 -0300

    na

--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBQhWkasOxoeArGLNv5L6-7qUm6TmL1Pxk",
+  authDomain: "reactportfoliopage.firebaseapp.com",
+  databaseURL: "https://reactportfoliopage-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "reactportfoliopage",
+  storageBucket: "reactportfoliopage.appspot.com",
+  messagingSenderId: "129693764255",
+  appId: "1:129693764255:web:5de96201e662c24b6d70aa"
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
